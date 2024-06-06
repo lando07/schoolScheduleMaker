f86bf0d6c24d91884511869270203770dfaaea78
lando07 <96402540+lando07@users.noreply.github.com>
2024-06-06 18:59:16 -0500

    surface level stuff

--- src/GradeLevel.java
+++ src/GradeLevel.java
@@ -1,3 +1,42 @@
+import java.util.ArrayList;
+
 public class GradeLevel {
-    
+    /**
+     * Stores students and all couse entries for that grade level
+     */
+    int level;
+
+    ArrayList<Student> students;
+    ArrayList<CourseEntry> entries;
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public ArrayList<CourseEntry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(ArrayList<CourseEntry> entries) {
+        this.entries = entries;
+    }
+
+    public GradeLevel(int level, ArrayList<Student> students, ArrayList<CourseEntry> entries) {
+        this.level = level;
+        this.students = students;
+        this.entries = entries;
+    }
+
 }
--- src/School.java
+++ src/School.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+
 public class School {
-    
+    /**
+     * The chonky class that stores everything
+     */
+    ArrayList<GradeLevel> grades;
+
+    public GradeLevel getGradeLevel(int gradeNum){
+
+    }
 }
